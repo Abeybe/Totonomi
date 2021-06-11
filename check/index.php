fed1603d79fb9d6c0a77ae6ddb7bbc47b6fc6643
Abeybe <dritter3d@gmail.om>
2021-06-11 09:05:05 +0900

    add default css

--- check/index.php
+++ check/index.php
@@ -16,13 +16,13 @@
     <link href="https://fonts.googleapis.com/earlyaccess/kokoro.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200&amp;display=swap" rel="stylesheet">
     
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/default.css">
 
     <script src="//cdn.webrtc.ecl.ntt.com/skyway-latest.js" ></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" ></script>
     <script src="//cdn.webrtc.ecl.ntt.com/skyway-latest.js" ></script>
 
-    <script src="script.js"></script>
+    <script src="/script.js"></script>
 
     <!-- <script src="js/face-api.min.js"></script> -->
     <!-- <script src="js/p5.min.js"></script> -->
--- invited/index.php
+++ invited/index.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.googleapis.com/earlyaccess/kokoro.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200&amp;display=swap" rel="stylesheet">
     
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/default.css">
 
     <script src="//cdn.webrtc.ecl.ntt.com/skyway-latest.js" ></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" ></script>
